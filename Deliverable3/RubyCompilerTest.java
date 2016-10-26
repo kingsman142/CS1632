a03b7019cf592f2ecb2e5a7264b9d0772c88d7be
kingsman142 <kingsman142@yahoo.com>
2016-10-25 23:13:36 -0400

    setup basic test

--- Deliverable3/RubyCompilerTest.java
+++ Deliverable3/RubyCompilerTest.java
@@ -1,20 +1,17 @@
-package com.example.tests;
-
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-public class RubyTests {
+public class RubyCompilerTest {
     private WebDriver driver = new HtmlUnitDriver();
 
     @Test
     public void testRubyTests() throws Exception {
-        driver.get(baseUrl + "/");
+        //driver.get(baseUrl + "/");
         driver.findElement(By.name("commit")).click();
     }
 }
