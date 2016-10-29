5f71820e221affdec5032853cb04b2e29daba034
kingsman142 <kingsman142@yahoo.com>
2016-10-28 23:46:38 -0400

    added two new tests for the default parse text and the title of the home page

--- Deliverable3/RubyCompilerTest.java
+++ Deliverable3/RubyCompilerTest.java
@@ -22,7 +22,6 @@ public class RubyCompilerTest {
         //Find all "commit" buttons on the page
         List<WebElement> commitButtons = driver.findElements(By.name("commit"));
         WebElement tokenizeButton = null;
-        System.out.println("length: " + commitButtons.size());
 
         //Find the commit button with the value "Tokenize"
         for(WebElement element : commitButtons){
@@ -42,5 +41,51 @@ public class RubyCompilerTest {
         String codeBlockText = codeBlock.getText();
 
         assertEquals("", codeBlockText);
+    }
+
+    //When a user is on the front page and clicks the "Parse"
+    //  button without entering any code into the code text area,
+    //  they should be redirected to a new page with the basic
+    //  templated for parsed code.
+    //In this case, the user should see two <code> elements
+    //  containing text relating to void_stmt, the default
+    //  template that the compiler feeds to the user.
+    @Test
+    public void ClickingParseButtonWithNoCodeShouldReturnBaseCodeBlock() {
+        driver.get("http://lit-bayou-7912.herokuapp.com/");
+
+        //Find all "commit" buttons on the page
+        List<WebElement> commitButtons = driver.findElements(By.name("commit"));
+        WebElement parseButton = null;
+
+        //Find the commit button with the value "Tokenize"
+        for(WebElement element : commitButtons){
+            String elementValue = element.getAttribute("value");
+            if(elementValue.equals("Parse")){
+                parseButton = element;
+                break;
+            }
+        }
+
+        //Make sure the button has been found
+        assertNotNull(parseButton);
+
+        parseButton.click();
+
+        List<WebElement> codeBlocks = driver.findElements(By.tagName("code"));
+        String firstCodeBlock = codeBlocks.get(0).getText();
+        String secondCodeBlock = codeBlocks.get(1).getText();
+        assertEquals("program [[:void_stmt]]", firstCodeBlock);
+        assertEquals("program --void_stmt", secondCodeBlock);
+    }
+
+    //When a user enters the home page of the Ruby compiler
+    //  website, the title of the website (the text that
+    //  appears on the browser's tab) should be "Hoodpopper".
+    @Test
+    public void HoodpopperWebsiteShouldHaveTitleOfHoodpopper(){
+        driver.get("http://lit-bayou-7912.herokuapp.com/");
+        String pageTitle = driver.getTitle();
+        assertEquals("Hoodpopper", pageTitle);
     }
 }
--- Deliverable3/RubyCompilerTestRunner.java
+++ Deliverable3/RubyCompilerTestRunner.java
@@ -5,44 +5,43 @@ import org.junit.runner.notification.*;
 
 public class RubyCompilerTestRunner {
     public static void main(String[] args) {
+    	ArrayList<Class> classesToTest = new ArrayList<Class>();
+    	boolean anyFailures = false;
 
-	ArrayList<Class> classesToTest = new ArrayList<Class>();
-	boolean anyFailures = false;
+    	// ADD ANY MORE CLASSES YOU WISH TO TEST HERE
 
-	// ADD ANY MORE CLASSES YOU WISH TO TEST HERE
+    	classesToTest.add(RubyCompilerTest.class);
 
-	classesToTest.add(RubyCompilerTest.class);
+    	// For all test classes added, loop through and use JUnit
+    	// to run them.
 
-	// For all test classes added, loop through and use JUnit
-	// to run them.
+    	for (Class c: classesToTest) {
+    	    Result r = JUnitCore.runClasses(c);
 
-	for (Class c: classesToTest) {
-	    Result r = JUnitCore.runClasses(c);
+    	    // Print out any failures for this class.
+            System.out.println();
+    	    for (Failure f : r.getFailures()) {
+    		    System.out.println(f.toString());
+    	    }
 
-	    // Print out any failures for this class.
+    	    // If r is not successful, there was at least one
+    	    // failure.  Thus, set anyFailures to true - this
+    	    // can never be set back to false (no amount of
+    	    // successes will ever eclipse the fact that there
+    	    // was at least one failure.
 
-	    for (Failure f : r.getFailures()) {
-		System.out.println(f.toString());
-	    }
+    	    if (!r.wasSuccessful()) {
+    		    anyFailures = true;
+    	    }
 
-	    // If r is not successful, there was at least one
-	    // failure.  Thus, set anyFailures to true - this
-	    // can never be set back to false (no amount of
-	    // successes will ever eclipse the fact that there
-	    // was at least one failure.
+    	}
 
-	    if (!r.wasSuccessful()) {
-		anyFailures = true;
-	    }
+    	// After completion, notify user if all tests passed or any failed.
 
-	}
-
-	// After completion, notify user if all tests passed or any failed.
-
-	if (anyFailures) {
-	    System.out.println("\n!!! - At least one failure, see above.");
-	} else {
-	    System.out.println("\nALL TESTS PASSED");
-	}
+    	if (anyFailures) {
+    	    System.out.println("\n!!! - At least one failure, see above.");
+    	} else {
+    	    System.out.println("\nALL TESTS PASSED");
+    	}
     }
 }
